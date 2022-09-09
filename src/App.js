cf8cb138d66305a7e12f360f3e14c585678ab756
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2022-09-09 22:00:26 +0100

    change color of funciton buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Button from './components/Button';
 import Input from './components/Input';
 
 const App = () => {
+
+  const buttonColor = "#f2a33c";
+
   return (
     <div className="App">
       <div className='calc-wrapper'>
@@ -11,29 +14,30 @@ const App = () => {
           <Button symbol="7" />
           <Button symbol="8" />
           <Button symbol="9" />
-          <Button symbol="/" />
+          <Button symbol="/" color={buttonColor} />
         </div>
         <div className='row'>
           <Button symbol="4" />
           <Button symbol="5" />
           <Button symbol="6" />
-          <Button symbol="*" />
+          <Button symbol="*" color={buttonColor} />
         </div>
         <div className='row'>
           <Button symbol="1" />
           <Button symbol="2" />
           <Button symbol="3" />
-          <Button symbol="+" />
+          <Button symbol="+" color={buttonColor} />
         </div>
         <div className='row'>
           <Button symbol="0" />
           <Button symbol="." />
           <Button symbol="=" />
-          <Button symbol="-" />
+          <Button symbol="-" color={buttonColor} />
         </div>
+        <Button symbol="Clear" color="red" />
       </div>
     </div>
   );
-}
+};
 
 export default App;
