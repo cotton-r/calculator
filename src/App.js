2482fa9a1b8d6e0150f8c4746ed05a9472fb3de7
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2022-09-09 23:23:08 +0100

    apply handleClick function to all necessary buttons

--- src/App.js
+++ src/App.js
@@ -21,27 +21,27 @@ const App = () => {
         <Input text={text} result={result} />
         <div className='row'>
           <Button symbol="7" handleClick={addToText} />
-          <Button symbol="8" />
-          <Button symbol="9" />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
           <Button symbol="/" color={buttonColor} />
         </div>
         <div className='row'>
-          <Button symbol="4" />
-          <Button symbol="5" />
-          <Button symbol="6" />
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
           <Button symbol="*" color={buttonColor} />
         </div>
         <div className='row'>
-          <Button symbol="1" />
-          <Button symbol="2" />
-          <Button symbol="3" />
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
           <Button symbol="+" color={buttonColor} />
         </div>
         <div className='row'>
-          <Button symbol="0" />
-          <Button symbol="." />
+          <Button symbol="0" handleClick={addToText} />
+          <Button symbol="." handleClick={addToText} />
           <Button symbol="=" />
-          <Button symbol="-" color={buttonColor} />
+          <Button symbol="-" color={buttonColor} handleClick={addToText} />
         </div>
         <Button symbol="Clear" color="#2b2b2b"/>
       </div>
